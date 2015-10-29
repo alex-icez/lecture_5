802411f50b14e6d71910fa6daa6e5d016c4027ae
alex-icez <alex-icex@mail.ru>
2015-10-29 19:48:00 +0300

    mess

--- src/lecture_5/Sample1.java
+++ src/lecture_5/Sample1.java
@@ -5,11 +5,11 @@ class Box {
 	int w;
 	int d;
 	
-/*	public Box(int h, int w, int d) {
+	public Box(int h, int w, int d) {
 		this.h = h;
 		this.w = w;
 		this.d = d;
-	}*/
+	}
 	
 	int value() {
 		return h * w * d;
@@ -33,8 +33,7 @@ class RealBox extends Box {
 public class Sample1 {
 
 	public static void main(String[] args) {
-		Box b = new Box();
-	
+		RealBox b = new RealBox(1,2,3,4);
 		System.out.println(b.ro());
 
 	}

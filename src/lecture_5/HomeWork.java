e16fa05fa197b284a07465d39d75a2784d35d1af
alex-icez <alex-icex@mail.ru>
2015-10-29 18:29:56 +0300

    mess

--- src/lecture_5/HomeWork.java
+++ src/lecture_5/HomeWork.java
@@ -5,8 +5,19 @@ class Memory {
 	int kbytes;
 	int mbytes;
 	
-	public Memory(int bytes, int kbytes, int mbytes) {
+	public Memory(int bytes) {
+		this(bytes, 0, 0);
+	}
 	
+	public Memory(int bytes, int kbytes, int mbytes) {
+		this.bytes = bytes % 1024;
+		this.kbytes = (kbytes + (bytes / 1024)) % 1024;
+		this.mbytes = mbytes + (kbytes + (bytes / 1024)) / 1024;
+	}
+	
+	public void print() {
+		
+		
 		
 	}
 }

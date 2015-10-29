0bcdafd942b21c3d86b659e6954096396bc15702
alex-icez <alex-icex@mail.ru>
2015-10-29 19:09:48 +0300

    mess

---
+++ src/lecture_5/Sample1.java
@@ -0,0 +1,18 @@
+package lecture_5;
+
+class Box {
+	int h;
+	int w;
+	int d;
+	
+}
+
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
